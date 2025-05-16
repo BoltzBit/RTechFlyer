22d00ca7bcf3a6635430fa16c73559c67dca30af
Mateus Ramos da Silva <45329846+BoltzBit@users.noreply.github.com>
2025-05-15 23:18:56 -0300

    teste do Pr (#6)
    * primeiro teste do ci/cd
    
    * corrigindo deploy dev
    
    * pipeline rodando no pull request
    
    * inserindo username
    
    * build: changing .yml files
    
    * build: fixing name of variable
    
    * build: adjustments to deploy
    
    * build: removing for now
    
    * build: fixing the yml
    
    * build: fixing deploy yml
    
    * build: fixing trigger for deploy
    
    * refactor: add todo
    
    * fix: docker compose and appsettings

--- API/Program.cs
+++ API/Program.cs
@@ -5,10 +5,15 @@ using API.AWS;
 using API.Model;
 using API.Services;
 using Microsoft.Extensions.Options;
+using DotNetEnv;
+
+Env.Load("../.env");
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Configuration.AddEnvironmentVariables();
+builder.Configuration
+    .AddJsonFile("appsettings.json")
+    .AddEnvironmentVariables();
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -32,11 +37,8 @@ builder.Services.AddScoped<IEmailService, EmailService>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
